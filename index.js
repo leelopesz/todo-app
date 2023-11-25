a0871aeaf6c028fadeea9e6b6b8e387b5f279522
leelopesz <bleticia15@yahoo.com>
2023-11-24 22:15:02 -0300

    Iniciando leitura dos dados do banco de dados

--- index.js
+++ index.js
@@ -36,7 +36,27 @@ app.post('/criar', (requisicao, resposta) => {
 })
 
 app.get('/',(requisicao, resposta) => {
-    resposta.render('home')
+    const sql = 'SELECT * FROM tarefas'
+
+    conexao.query(sql, (erro, dados) =>{
+        if (erro) {
+            return console.log(erro)
+        }
+
+        console.log(dados)
+
+        const tarefas = dados.map((dado) => {
+            return {
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: dado.completa === 0 ? false : true
+            }
+        })
+
+        console.log(tarefas)
+    })
+    
+    resposta.render('home') 
 })
 
 const conexao = mysql.createConnection({
